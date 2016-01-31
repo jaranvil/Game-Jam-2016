7d5ad06c4bd0b42ecdc636c1e77cd4ce271c506a
ANNA-MARIE-ACER\AnnaMarie <amleb.ace@gmail.com>
2016-01-31 00:35:15 -0400

    added script for collectables

---
+++ DirectileDisfunctionUnity/Directile Dysfunction/Assets/Scripts/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Collectable : MonoBehaviour
+{
+    private float bonus = 2;
+
+    public float getBonus()
+    {
+        return bonus;
+    }
+    public void setBonus(float bonus)
+    {
+        this.bonus = bonus;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            GameManager.instance.AdjustPurpose(bonus);
+            GameManager.instance.Message("I know what I must do ! ..I think");
+        }
+    }
+}
---
+++ DirectileDisfunctionUnity/Directile Dysfunction/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime); ;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+    }
+
+    
+}
+//Destroy(other.gameObject);
